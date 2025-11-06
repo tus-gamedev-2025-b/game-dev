7c132601f967a26b6a7a6c2b10995522bdba7758
rai <96561881+r4ai@users.noreply.github.com>
2025-11-06 19:52:07 +0900

    fix: Stop TextMeshPro's dynamic font asset from generating diffs on every run

---
+++ tanks/Assets/Editor/DynamicFontCleaner.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using UnityEditor;
+
+/// <summary>
+/// TextMeshProのDynamicなフォントアセットの差分が毎実行時に出るのをなくす。
+/// cf. https://www.stmn.tech/entry/2024/02/14/025205
+/// </summary>
+[InitializeOnLoad]
+public static class DynamicFontCleaner
+{
+    static DynamicFontCleaner()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state != PlayModeStateChange.ExitingPlayMode) return;
+        var tmpFontAssets = Resources.FindObjectsOfTypeAll<TMPro.TMP_FontAsset>();
+        foreach (var tmpFontAsset in tmpFontAssets)
+        {
+            if (tmpFontAsset == null || tmpFontAsset.atlasPopulationMode != AtlasPopulationMode.Dynamic)
+                continue;
+            tmpFontAsset.ClearFontAssetData();
+            Debug.Log("DynamicFontCleaner: ClearFontAssetData " + tmpFontAsset.name);
+        }
+    }
+}
