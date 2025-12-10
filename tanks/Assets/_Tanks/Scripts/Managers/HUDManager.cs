a708da3f84426b25e7d5d269dd28849cb88e7034
tus6322074 <6322074@ed.tus.ac.jp>
2025-12-10 19:46:15 +0900

    Revert last two commits to restore state at HEAD~2

--- tanks/Assets/_Tanks/Scripts/Managers/HUDManager.cs
+++ tanks/Assets/_Tanks/Scripts/Managers/HUDManager.cs
@@ -13,33 +13,16 @@ public class HUDManager : MonoBehaviour
     [Tooltip("Reference to the GameManager")]
     [SerializeField] public GameManager m_GameManager;
 
-    [SerializeField]
-    private Camera player1Camera;
-
     public void Start()
     {
-        // プレイヤー1の戦車プレハブから MinimapCamera を取得
-        var cam = m_GameManager.m_Tank1Prefab.GetComponentInChildren<Camera>(true);
-        player1Camera = cam;
-
         m_StockP1.gameObject.SetActive(false);
         m_StockP2.gameObject.SetActive(false);
-
-        // TankPrefab を配列にまとめる（存在しない場合は null のまま）
-        var tankPrefabs = new[]
-        {
-            m_GameManager.m_Tank1Prefab,
-            m_GameManager.m_Tank2Prefab,
-            m_GameManager.m_Tank3Prefab,
-            m_GameManager.m_Tank4Prefab
-        };
 
         m_GameManager.OnGameLoopStateChanged += HandleGameLoopStateChanged;
         foreach (var tank in m_GameManager.m_SpawnPoints)
         {
             // 新しいWeaponStockData対応のイベントを購読
             tank.OnWeaponStockChanged += HandleWeaponStockChanged;
-            tank.OnHealthChanged += HandleHealthChanged;
         }
     }
 
@@ -66,22 +49,27 @@ public class HUDManager : MonoBehaviour
     {
         if (state == GameManager.GameLoopState.RoundPlaying)
         {
-            // プレイヤー1の実体を探す
-            var p1 = m_GameManager.m_Players.FirstOrDefault(t => t.ControlIndex == 1);
-            if (p1 != null)
+            if (DoesP1Exist()) m_StockP1.gameObject.SetActive(true);
+            if (DoesP2Exist()) m_StockP2.gameObject.SetActive(true);
+
+            if (m_GameManager.m_Players == null) return;
+            foreach (var tank in m_GameManager.m_Players)
             {
-                // インスタンスから MinimapCamera を取得
-                player1Camera = p1.m_Instance.transform.GetComponentInChildren<Camera>(true);
-                if (player1Camera != null)
+                switch (tank.ControlIndex)
                 {
-                    player1Camera.enabled = true;
+                    case 1:
+                        m_StockP1.InitPlayerStock(tank.MaxShellStock, tank.ShellStock);
+                        break;
+                    case 2:
+                        m_StockP2.InitPlayerStock(tank.MaxShellStock, tank.ShellStock);
+                        break;
                 }
             }
         }
         else
         {
-            if (player1Camera != null)
-                player1Camera.enabled = false;
+            m_StockP1.gameObject.SetActive(false);
+            m_StockP2.gameObject.SetActive(false);
         }
     }
 
@@ -99,19 +87,6 @@ public class HUDManager : MonoBehaviour
                 break;
             case 2:
                 m_StockP2.UpdatePlayerStock(stockData);
-                break;
-        }
-    }
-
-    private void HandleHealthChanged(int controlIndex, float value)
-    {
-        switch (controlIndex)
-        {
-            case 1:
-                m_StockP1.UpdateHP(value);
-                break;
-            case 2:
-                m_StockP2.UpdateHP(value);
                 break;
         }
     }
